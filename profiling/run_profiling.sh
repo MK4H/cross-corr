9cec2408fcaec08bb3196a568ea396fef36d604a
Karel Madera <karel.madera@eyen.se>
2022-02-07 17:19:33 +0100

    Profiling the naive orig and shuffle algorithsm
    For smaller inputs up to 128, the number of active warps is too low and
    we should increase it. This is true for both the orig and shuffle
    algorithms.
    
    One possibility is to use multiple threads per shift, splitting the rows
    for the given shift between multiple threads. This distribution could be
    dynamic based on the number of rows per given shift, as for example in
    64x64 shift -63,-63 computes only a single row, whereas shift 0,0 goes
    through all 64 rows of input.
    
    The shuffle algorithm bottleneck is the LSU execution unit, which has
    more than 90% usage for all input sizes. This is caused by each
    multiplication and sum requiring 3 shuffles. We could reduce this
    disparity by reading more right matrices into registers and computing
    sums for multiple input right matrices at once. This would reduce the
    ration to 4 shuffles per 2 multiplication additions, and this can be
    repeated again and again.
    
    These two optimizations can be combined.

--- profiling/run_profiling.sh
+++ profiling/run_profiling.sh
@@ -4,6 +4,18 @@ set -eu -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+if [[ $# -ne 1 ]]
+then
+	echo "Usage: $0 <algorithm>" >& 2
+	exit 1
+fi
+
+
 cd "${DIR}/../build"
 
-/opt/nvidia/nsight-compute/2021.2.1/ncu --set full --export "${DIR}/nai_shuffle" ./cross run nai_warp_shuffle_one_to_one ../data/ina_64_64_1_1.csv ../data/ina_64_64_1_2.csv
+ALG="$1"
+
+for SIZE in 64 128 256 512
+do
+    /opt/nvidia/nsight-compute/2021.2.1/ncu --set full --export "${DIR}/${ALG}_${SIZE}" ./cross run "${ALG}_one_to_one" "../data/ina_${SIZE}_${SIZE}_1_1.csv" "../data/ina_${SIZE}_${SIZE}_1_2.csv"
+done
