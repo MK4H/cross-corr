767d34f4fecb9f892025a51de74524ba4c1a6bf4
Karel Madera <karel.madera@eyen.se>
2022-02-07 15:53:52 +0100

    Fix shuffle implementation
    The shuffle implementation is now functioning. I just made it
    unnecessarily too complicated, it really only needed to properly compute
    the warp_x_right_start and warp_x_right_end.
    
    Now we always go from left to right over the matrix, broadcasting from
    the right matrix and shuffling the left matrix. If broadcast value does
    not overlap in the shift computed by the current thread, the thread will
    load 0 as it should access the left matrix out of bounds and thus is
    ignored. With shuffling, it will receive the correct values the moment
    the computation reaches it's shift.
    
    Remove debug prints from nainve_shuffle.cu.
    
    Add profiling of the current implementations of original and shuffle
    algorithsm with the 64x64x1x1 inputs. With this size of matrix, there is
    a problem in occupancy which I will try to solve in future commits.
    
    WARNING: Do not profile or benchmark with -G and other flags in
    CMakeLists.txt. It makes CUDA code abuse local memory and slows it down.
    Always check that we are compiling at most with -lineinfo for the
    profiler.

--- profiling/run_profiling.sh
+++ profiling/run_profiling.sh
@@ -6,4 +6,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "${DIR}/../build"
 
-/opt/nvidia/nsight-compute/2021.2.1/ncu --set full --export ./profile ./cross run nai_orig_n_to_mn ../data/ina_512_512_2_1.csv ../data/ina_512_512_4_1.csv
+/opt/nvidia/nsight-compute/2021.2.1/ncu --set full --export "${DIR}/nai_shuffle" ./cross run nai_warp_shuffle_one_to_one ../data/ina_64_64_1_1.csv ../data/ina_64_64_1_2.csv
