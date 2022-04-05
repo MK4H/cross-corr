66dd22bdfc5c0e2239d0e9484c285b397809ea8a
Karel Madera <karel.madera@eyen.se>
2022-04-05 17:00:55 +0200

    Add newer profiling with the current implementations
    Fix invalid argument loaded in n_to_m optimized naive alg. INstead of
    reading the left_matrices_per_thread, it read right_matrices_per_thread
    for both left and right matrices. JUst a copy paste error.
    
    Try fixing nvcc pushing the sum, thread_right, thread_left_top and
    thread_left_bottom into local memory in multileft and n_to_m optimized
    naive algorithms. These changes have no effect on this problem, but are
    generally good to do so I leave them in. THe changes are reduction of
    the number of rows, to reduce the number of registers used and to reduce
    compilation times. Even with only two left and right rows, the n_to_m
    kernel does not put the arrays into registers.

--- profiling/run_profiling.sh
+++ profiling/run_profiling.sh
@@ -4,18 +4,13 @@ set -eu -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-if [[ $# -ne 1 ]]
-then
-	echo "Usage: $0 <algorithm>" >& 2
-	exit 1
-fi
-
-
 cd "${DIR}/../build"
 
-ALG="$1"
 
-for SIZE in 16 32 64 128 256 512
+for ALG in nai_orig nai_warp_shuffle nai_warp_shuffle_work_distribution nai_multirow_shuffle nai_multileft_shuffle nai_shift_per_warp nai_shift_per_warp_shared_mem_rows
 do
-    /opt/nvidia/nsight-compute/2021.2.1/ncu --set full --export "${DIR}/${ALG}_${SIZE}" ./cross run "${ALG}_one_to_one" "../data/ina_${SIZE}_${SIZE}_1_1.csv" "../data/ina_${SIZE}_${SIZE}_1_2.csv"
+	for SIZE in 64 128 256 512
+	do
+		/opt/nvidia/nsight-compute/2021.2.1/ncu --set full --export "${DIR}/${ALG}_${SIZE}" ./cross run "${ALG}_one_to_one" "../data/ina_${SIZE}_${SIZE}_1_1.csv" "../data/ina_${SIZE}_${SIZE}_1_2.csv"
+	done
 done
